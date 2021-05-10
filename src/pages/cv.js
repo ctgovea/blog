a32e7e2d919f54fb6dc28646173949b69c7b2a4e
govea <ctgovea@gmail.com>
2021-05-10 15:38:51 -0500

    update object

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,7 +2,11 @@ import React from "react"
 
 const CVPage = () => (
     <>
-        <object width="100%" height="400" data="./cv.pdf" type="application/pdf">   </object>
+        <object data="./cv.pdf" type="application/pdf" width="700px" height="700px">
+            <embed src="cv.pdf">
+                <p>This browser does not support PDFs. Please download the PDF to view it: <a href="cv.pdf">Download PDF</a>.</p>
+            </embed>
+        </object>
     </>
 )
 
