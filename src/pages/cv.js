b2c7c71d1b43a8aecfafbf92646732ee717086a4
govea <ctgovea@gmail.com>
2021-05-10 15:32:30 -0500

    update data

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,11 +1,8 @@
 import React from "react"
-import downloadFile from '../../static/cv.pdf'
 
 const CVPage = () => (
     <>
-        <object data={downloadFile} type="application/pdf" width="100%" height="100%">
-            <p>You can download the <a href={downloadFile}>cv file</a></p>
-        </object>
+        <object width="100%" height="400" data="./cv.pdf" type="application/pdf">   </object>
     </>
 )
 
