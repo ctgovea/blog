fde5a51f99a29d757b8ba1a90f849dd36deefa42
govea <ctgovea@gmail.com>
2021-05-10 15:24:03 -0500

    object tag for pdf

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -3,7 +3,9 @@ import downloadFile from '../../static/cv.pdf'
 
 const CVPage = () => (
     <>
-        <a href={downloadFile} download>Download the directly imported file</a>{` `}
+        <object data={downloadFile} type="application/pdf" width="100%" height="100%">
+            <p>You can download the <a href={downloadFile}>cv file</a></p>
+        </object>
     </>
 )
 
